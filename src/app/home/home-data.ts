6663fe4f4196666f774683bcf433c61ea0fc4d1b
lucasgiosso <lucasdiazgiosso@gmail.com>
2024-04-28 20:52:43 -0300

    Sprint 2 Entregado

--- src/app/home/home-data.ts
+++ src/app/home/home-data.ts
@@ -11,29 +11,29 @@ export const navbarData = [
         label: 'Quien Soy'
     },
 
-    // {
-    //     routeLink: 'ahorcado',
-    //     icon: 'fab fa-nimblr', 
-    //     label: 'Ahorcado'
-    // },
+    {
+        routeLink: 'ahorcado',
+        icon: 'fab fa-nimblr', 
+        label: 'Ahorcado'
+    },
 
-    // {
-    //     routeLink: 'mayorOMenor',
-    //     icon: 'fa solid fa-code',
-    //     label: 'Mayor o Menor'
-    // },
+    {
+        routeLink: 'mayorOMenor',
+        icon: 'fa solid fa-code',
+        label: 'Mayor o Menor'
+    },
 
-    // {
-    //     routeLink: 'preguntados',
-    //     icon: 'fa solid fa-question',
-    //     label: 'Preguntados'
-    // },
+    {
+        routeLink: 'preguntados',
+        icon: 'fa solid fa-question',
+        label: 'Preguntados'
+    },
 
-    // {
-    //     routeLink: 'word-scramble',
-    //     icon: 'fa solid fa-spell-check',
-    //     label: 'Preguntados'
-    // },
+    {
+        routeLink: 'word-scramble',
+        icon: 'fa solid fa-spell-check',
+        label: 'Word Scramble'
+    },
 
     {
         routeLink: 'logout',
